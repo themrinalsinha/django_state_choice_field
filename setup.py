ef83db97621961cb37d2a3f0fced54585f619e1a
Mrinal Sinha <mail@themrinalsinha.com>
2023-11-04 05:48:28 +0530

    created license for open and free use

--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ setup(
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     description = "A Django enum field",
     long_description = readme,
